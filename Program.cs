8d181d4225f6434ec5ec5245b7565f2a917ac038
romankashmanov <110128709+romankashmanov@users.noreply.github.com>
2022-08-31 13:33:27 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,76 @@
+﻿/*
+
+//Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N
+
+Console.Write("Введи N: ");
+int N = Convert.ToInt16(Console.ReadLine());
+Console.Write("Введи M: ");
+int M = Convert.ToInt16(Console.ReadLine());
+Console.WriteLine($"N={N}, M={M}");
+if (M>N)
+for (int i = N; i <= M; i++)
+    Console.Write($" {i}");
+else
+    for (int i = M; i <= N; i++)
+        Console.Write($" {i}");
+*/
+
+/*
+
+// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+
+int m = InputNumbers("Введите m: ");
+int n = InputNumbers("Введите n: ");
+int temp = m;
+
+if (m > n) 
+{
+  m = n; 
+  n = temp;
+}
+
+PrintSumm(m, n, temp=0);
+
+void PrintSumm(int m, int n, int summ)
+{
+  summ = summ + n;
+  if (n <= m)
+  {
+    Console.Write($"Сумма элементов= {summ} ");
+    return;
+  }
+  PrintSumm(m, n - 1, summ);
+}
+
+int InputNumbers(string input) 
+{
+  Console.Write(input);
+  int output = Convert.ToInt32(Console.ReadLine());
+  return output;
+}
+
+*/
+
+/*
+ 
+ //Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+
+Console.WriteLine("Введите начальное число M:");
+int numberM = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите начальное число N:");
+int numberN = int.Parse(Console.ReadLine());
+
+///Метод вычисления функции Аккермана:
+int AckermannFunction (int numberM, int numberN)
+{
+    if (numberM == 0) return numberN + 1;
+    if (numberM != 0 && numberN == 0) return AckermannFunction(numberM - 1, 1);
+    if (numberM > 0 && numberN > 0) return AckermannFunction(numberM - 1, AckermannFunction(numberM, numberN - 1));
+return AckermannFunction(numberM, numberN);
+}
+
+Console.WriteLine($"Функция Аккермана для чисел A({numberM},{numberN}) = {AckermannFunction(numberM, numberN)}");
+
+*/
